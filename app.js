83f678ceabbd1dbb6ccf628c31c419ae86b9c2a2
Siarhei Karol <m.jjesire@gmail.com>
2021-01-27 11:13:37 +0300

    fix:add-formatted-events

--- app.js
+++ app.js
@@ -11,20 +11,25 @@ let access_token = '';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// Startpage
+// Server flow init redirect
 app.get('/', (req, res) => {
   res.redirect('https://secure.meetup.com/oauth2/authorize?client_id=8jhvj9qp5mc408od5bujmridfk&response_type=code&redirect_uri=http://localhost:3000/login');
 })
 
-// Login
+// Login part gets authorization_code and makes post request for obtaing access_token
 app.get('/login', (req, res) => {
+
+  // Response from meetup.com/oauth2/authorize with code or state
+  const code = req.query.code;
+
+  // Required values for getting access_token in next step:
   const url = 'https://secure.meetup.com/oauth2/access';
   const client_id = '8jhvj9qp5mc408od5bujmridfk';
   const client_secret = 'c1qppfno81ipsktmhfc7bc2i66';
   const grant_type = 'authorization_code';
   const redirect_uri = 'http://localhost:3000/login';
-  const code = req.query.code;
 
+  // Make urlencoded query of parameters for request
   const params = new URLSearchParams();
   params.append('client_id', client_id);
   params.append('client_secret', client_secret);
@@ -32,17 +37,19 @@ app.get('/login', (req, res) => {
   params.append('redirect_uri', redirect_uri);
   params.append('code', code);
 
+  // Configure header
   const config = {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     }
   };
 
-  // make post request
+  // Making post request via axios
   axios.post(url, params, config)
     .then((response) => {
       access_token = response.data.access_token;
-      // View forms
+
+      // Show form.html
       res.sendFile(path.join(__dirname+'/views/form.html'));
     })
     .catch((err) => {
@@ -50,16 +57,21 @@ app.get('/login', (req, res) => {
     })
 })
 
+// upcoming_events makes api request with json response
 app.post('/upcoming_events', (req, res) => {
+  // parameters for upcoming_events request
   const url = 'https://api.meetup.com/find/upcoming_events';
+
+  // Geo location of city
   const latitude = '37.77';
   const longitude = '-122.41';
-  // Get data from forms
+
+  // Getting startdate, enddate, text_filter from form.html
   const startdate = req.body.startdate+'T00:00:00.000';
   const enddate = req.body.enddate+'T00:00:00.000';
   const text_filter = req.body.filter;
 
-  // make axios req
+  // Header { Authorization: Bearer {access_token}}
   const config = {
     headers: {
       'Authorization' : 'Bearer ' + access_token
@@ -72,11 +84,25 @@ app.post('/upcoming_events', (req, res) => {
       text: text_filter
     }
   };
-  // make get request
+  // make get request to find upcoming_events
   axios.get(url, config)
     .then((response) => {
-      res.send(response.data.events);
-      console.log(response.data.events);
+      const events = response.data.events;
+
+      // get local_time, name, description for events_formatted
+      var events_formatted = new Array();
+      events.forEach((item, i) => {
+        events_formatted.push({
+          'id' : i,
+          'local_time': item.local_time,
+          'local_date' : item.local_date,
+          'name' : item.name,
+          'description' : item.description
+        })
+        console.log(i, item.local_time, item.local_date, item.name);
+      });
+
+      res.send(events_formatted);
     })
     .catch((err) => {
       res.send(err);
